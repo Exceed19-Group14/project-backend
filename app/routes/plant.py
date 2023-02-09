597790b55dee76c083e8812075c4f8bb6cdbcf2c
Supakrit Aphonmaeklong <92836314+Supakrit65@users.noreply.github.com>
2023-02-09 16:32:26 +0700

    add plant base model

--- app/routes/plant.py
+++ app/routes/plant.py
@@ -9,6 +9,29 @@ plant_router = APIRouter(
     prefix='/plant'
 )
 
+
+class ModeEnum(IntEnum):
+    auto = 1
+    manual = 0
+
+
+class ForceWaterEnum(IntEnum):
+    active = 1
+    inactive = 0
+    
+    
+class PlantModel(BaseModel):
+    name: str
+    mode: ModeEnum
+    moisture: int
+    temperature: int
+    light: int
+    targeted_temperature: int
+    targeted_moisture: int
+    targeted_light: int
+    force_water: ForceWaterEnum
+    
+
 @plant_router.get('/')
 def show_plants():
     return {"msg": "there are 3 plants"}
