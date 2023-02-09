6ad1dffff2b9ad2f46dc8254892501e487ae51cb
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2023-02-09 21:40:15 +0700

    feat: Send watering status to hardware

--- app/routes/plant.py
+++ app/routes/plant.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from pydantic import BaseModel
 from app.routes.board import BoardModel
-from app.db.mongo import db, plant_collection
+from app.db.mongo import db, plant_collection, board_collection
 from enum import IntEnum
-from typing import List
+from typing import List, Union
 
 
 PLANT_COLLECTION = "plant"
@@ -22,10 +22,10 @@ class ModeEnum(IntEnum):
 class ForceWaterEnum(IntEnum):
     active = 1
     inactive = 0
-    
-    
+
+
 class PlantModel(BaseModel):
-    board: BoardModel
+    board: Union[BoardModel, int, None] = None
     name: str
     mode: ModeEnum
     moisture: int
@@ -35,7 +35,13 @@ class PlantModel(BaseModel):
     targeted_moisture: int
     targeted_light: int
     force_water: ForceWaterEnum
-    
+    watering_time: int  # in secs
+
+    def find_board(self):
+        self.board = board_collection.find_one({
+            "board": self.board
+        })
+
 
 @plant_router.get('/')
 def show_plants():
@@ -48,4 +54,27 @@ def create_plant(dbo: PlantModel):
     plant_collection.insert_one({
         dbo.dict()
     })
-    
+
+
+@plant_router.get('/{id}/water')
+def get_water_command(id: int) -> int:
+    doc = plant_collection.find_one({
+        "board": id
+    })
+
+    if doc is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, {
+            "error": {
+                "details": "Not found"
+            }
+        })
+
+    if doc['mode'] == ModeEnum.auto:
+        # TODOS Condition to watering some plant
+        pass
+    else:
+        if doc['force_water'] == ForceWaterEnum.active:
+            return 1
+        else:
+            return 0
+    return 0
