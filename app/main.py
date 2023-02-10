bd6dd78ad088c7c34861c4b759151ce71486da29
Supakrit Aphonmaeklong <supakritaphonmaeklong@gmail.com>
2023-02-10 10:34:20 +0700

    Plant management (#16)
    * register plant router
    
    * add plant base model
    
    * const name for plant collection
    
    * feat: Create & Get all boards
    
    * add create plant
    
    * Fix pydantic objectid
    
    * add collection val
    
    * rename plant router
    
    * exit post bug path
    
    * implement createplant model
    
    * get plant by board id
    
    * basic endpoint
    
    ---------
    
    Co-authored-by: Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.board import router as BoardRouter
-from app.routes.plant import plant_router as PlantRouter
+from app.routes.plant import router as PlantRouter
 
 
 app = FastAPI()
--- app/routes/plant.py
+++ app/routes/plant.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, status, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from app.routes.board import BoardModel
 from app.db.mongo import db, plant_collection, board_collection
+from app.utils.objectid import PydanticObjectId
+from bson.objectid import ObjectId
 from enum import IntEnum
+from datetime import datetime
 from typing import List, Union
 
 
 PLANT_COLLECTION = "plant"
 
 
-plant_router = APIRouter(
+router = APIRouter(
     prefix='/plant'
 )
 
@@ -22,44 +25,129 @@ class ModeEnum(IntEnum):
 class ForceWaterEnum(IntEnum):
     active = 1
     inactive = 0
+    
 
-
-class PlantModel(BaseModel):
-
-    board: Union[BoardModel, int, None] = None
-    name: str
-    mode: ModeEnum
+class UpdateMoisture(BaseModel):
     moisture: int
-    temperature: int
+
+
+class UpdateTemperature(BaseModel):
+    temperature: float
+    
+
+class UpdateLight(BaseModel):
     light: int
-    targeted_temperature: int
+
+
+class UpdateModeAuto(BaseModel):
+    mode: ModeEnum
+    targeted_temperature: float
     targeted_moisture: int
     targeted_light: int
-    force_water: ForceWaterEnum
-    watering_time: int  # in miliseconds
+  
 
-    def find_board(self):
-        self.board = board_collection.find_one({
-            "board": self.board
-        })
+class UpdateModeManual(BaseModel):
+    mode: ModeEnum
+    watering_time: int  # in miliseconds
 
 
 class WaterStatusResponse(BaseModel):
     water_status: ForceWaterEnum
     duration: Union[int, None]  # in miliseconds
+  
+  
+class PlantModel(BaseModel):
+    plant_id: int
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias='_id')
+    board: Union[None, int]
+    name: str
+    mode: ModeEnum
+    moisture: Union[int, None] = None
+    temperature: Union[float, None] = None
+    light: Union[int, None] = None
+    targeted_temperature: Union[int, None] = None
+    targeted_moisture: Union[int, None] = None
+    targeted_light: Union[int, None] = None
+    force_water: ForceWaterEnum = ForceWaterEnum.inactive
+    watering_time: int # in secs
+    
+    def find_board(self):
+        self.board = board_collection.find_one({
+            "board": self.board
+        })
 
+class CreatePlant(BaseModel):
+    board: int
+    plant_id: int
+    name: str
+    date: datetime
+    mode: ModeEnum
+    targeted_temperature: Union[float, None] = None
+    targeted_moisture: Union[int, None] = None
+    targeted_light: Union[int, None] = None
+   
 
-@plant_router.get('/')
+@router.get('/', response_model=List[PlantModel])
 def show_plants():
-    return {"msg": "there are 3 plants"}
+    return list(plant_collection.find({}))
 
 
-@plant_router.post('/plant', status_code=status.HTTP_201_CREATED)
-def create_plant(dbo: PlantModel):
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_plant(dbo: CreatePlant):
     """add new plant into the database"""
-    plant_collection.insert_one({
-        dbo.dict()
-    })
+    plant_collection.insert_one(dbo.dict())
+    
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=PlantModel)
+def get_plant(id: int):
+    """get a specify plant info by board id"""
+    plant = plant_collection.find_one({"plant_id": id})
+    if plant is not None:
+        return plant
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f"Plant with ID {id} not found"
+    )
+    
+
+@router.put('/{id}/moisture')
+def update_moisture(id: int, dbo: UpdateMoisture):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"moisture": dbo.dict().get('moisture')}}
+        )
+    
+
+@router.put('/{id}/temperature')
+def update_temperature(id: int, dbo: UpdateTemperature):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"temperature": dbo.dict().get('temperature')}}
+        )
+
+
+@router.put('/{id}/light')
+def update_light(id: int, dbo: UpdateLight):
+        plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"light": dbo.dict().get('light')}}
+        )
+
+
+@router.put('/{id}/mode/auto')
+def update_mode(id: int, dbo: UpdateModeAuto):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": dbo.dict()}
+        )
+
+
+@router.put('/{id}/mode/manual')
+def update_mode(id: int, dbo: UpdateModeManual):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"mode": dbo.dict().get("mode")}})
+
+
+@router.put('/{id}/unregister')
+def unregister_plant(id: int):
+    plant_collection.update_one(
+        {"plant_id": id}, {"$set": {"board": None}})
 
 
 @plant_router.get('/{id}/water')
