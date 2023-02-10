3c197018af6ff1e5c10b5157c24d550086fade37
Supakrit Aphonmaeklong <92836314+Supakrit65@users.noreply.github.com>
2023-02-10 10:22:57 +0700

    basic endpoint

--- app/routes/plant.py
+++ app/routes/plant.py
@@ -25,15 +25,39 @@ class ModeEnum(IntEnum):
 class ForceWaterEnum(IntEnum):
     active = 1
     inactive = 0
+    
+
+class UpdateMoisture(BaseModel):
+    moisture: int
+
+
+class UpdateTemperature(BaseModel):
+    temperature: float
+    
+
+class UpdateLight(BaseModel):
+    light: int
+
+
+class UpdateModeAuto(BaseModel):
+    mode: ModeEnum
+    targeted_temperature: float
+    targeted_moisture: int
+    targeted_light: int
+    
+
+class UpdateModeManual(BaseModel):
+    mode: ModeEnum
 
 
 class PlantModel(BaseModel):
+    plant_id: int
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias='_id')
     board: Union[None, int]
     name: str
     mode: ModeEnum
     moisture: Union[int, None] = None
-    temperature: Union[int, None] = None
+    temperature: Union[float, None] = None
     light: Union[int, None] = None
     targeted_temperature: Union[int, None] = None
     targeted_moisture: Union[int, None] = None
@@ -43,10 +67,11 @@ class PlantModel(BaseModel):
 
 class CreatePlant(BaseModel):
     board: int
+    plant_id: int
     name: str
     date: datetime
     mode: ModeEnum
-    targeted_temperature: Union[int, None] = None
+    targeted_temperature: Union[float, None] = None
     targeted_moisture: Union[int, None] = None
     targeted_light: Union[int, None] = None
 
@@ -65,12 +90,50 @@ def create_plant(dbo: CreatePlant):
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=PlantModel)
 def get_plant(id: int):
     """get a specify plant info by board id"""
-    plant = plant_collection.find_one({"board": id})
+    plant = plant_collection.find_one({"plant_id": id})
     if plant is not None:
         return plant
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f"Plant with board ID {id} not found"
+        detail=f"Plant with ID {id} not found"
     )
+    
+
+@router.put('/{id}/moisture')
+def update_moisture(id: int, dbo: UpdateMoisture):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"moisture": dbo.dict().get('moisture')}}
+        )
+    
+
+@router.put('/{id}/temperature')
+def update_temperature(id: int, dbo: UpdateTemperature):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"temperature": dbo.dict().get('temperature')}}
+        )
 
 
+@router.put('/{id}/light')
+def update_light(id: int, dbo: UpdateLight):
+        plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"light": dbo.dict().get('light')}}
+        )
+
+
+@router.put('/{id}/mode/auto')
+def update_mode(id: int, dbo: UpdateModeAuto):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": dbo.dict()}
+        )
+
+
+@router.put('/{id}/mode/manual')
+def update_mode(id: int, dbo: UpdateModeManual):
+    plant_collection.update_one(
+            {"plant_id": id}, {"$set": {"mode": dbo.dict().get("mode")}})
+
+
+@router.put('/{id}/unregister')
+def unregister_plant(id: int):
+    plant_collection.update_one(
+        {"plant_id": id}, {"$set": {"board": None}})
